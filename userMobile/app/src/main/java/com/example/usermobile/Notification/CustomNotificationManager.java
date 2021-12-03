3433655e785d1deb5a7a9c8211098d2c268de664
Bejan Andrei <bejan.andrei.official@gmail.com>
2021-12-03 23:39:38 +0200

    NotificationSystem

---
+++ userMobile/app/src/main/java/com/example/usermobile/Notification/CustomNotificationManager.java
@@ -0,0 +1,48 @@
+package com.example.usermobile.Notification;
+
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CustomNotificationManager extends AppCompatActivity {
+    static String CHANNEL_ID = "channel 2";
+
+    Context context;
+    public CustomNotificationManager(Context context) {
+        this.context = context;
+        createNotificationChannel();
+    }
+
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Example Service Channel",
+                    android.app.NotificationManager.IMPORTANCE_HIGH
+            );
+
+            android.app.NotificationManager manager = context.getSystemService(android.app.NotificationManager.class);
+            if (manager != null) {
+                manager.createNotificationChannel(serviceChannel);
+            }
+        }
+    }
+
+    public void sendNotification(CustomNotification notification) {
+        Intent intent = new Intent(context, NotificationBroadcast.class);
+        intent.putExtra("id", notification.id);
+        intent.putExtra("title", notification.title);
+        intent.putExtra("text", notification.text);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, notification.timeToRing, pendingIntent);
+    }
+}
