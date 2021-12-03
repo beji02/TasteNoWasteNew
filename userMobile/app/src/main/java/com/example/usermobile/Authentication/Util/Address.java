1d52ea4d414032b96686cd72b3f16a38cd7e24e7
Bejan Andrei <bejan.andrei.official@gmail.com>
2021-12-04 01:00:19 +0200

    AdditionalDataXML

--- userMobile/app/src/main/java/com/example/usermobile/Authentication/Util/Address.java
+++ userMobile/app/src/main/java/com/example/usermobile/Authentication/Util/Address.java
@@ -1,16 +1,14 @@
 package com.example.usermobile.Authentication.Util;
 
 public class Address {
-    public String street;
-    private String house;
+    private String Address;
     private String city;
     private String county;
     private String country;
     private String zipCode;
 
-    public Address(String street, String house, String city, String county, String country, String zipCode) {
-        this.street = street;
-        this.house = house;
+    public Address(String address, String city, String county, String country, String zipCode) {
+        this.Address = address;
         this.city = city;
         this.county = county;
         this.country = country;
