b307f495236f4c684110d0970096a155bfd43254
Catalin-2002 <catalin_brita@iCloud.com>
2021-12-05 03:29:35 +0100

    Removed duplicates from database

--- userMobile/app/src/main/java/com/example/usermobile/DatabaseManager/DatabaseStorageManager.java
+++ userMobile/app/src/main/java/com/example/usermobile/DatabaseManager/DatabaseStorageManager.java
@@ -3,8 +3,19 @@ package com.example.usermobile.DatabaseManager;
 import android.content.Context;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.example.usermobile.R;
 import com.example.usermobile.Storage.Product;
+import com.example.usermobile.Storage.StorageListAdapter;
+import com.example.usermobile.Storage.StorageListView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class DatabaseStorageManager {
     Context context;
@@ -15,15 +26,42 @@ public class DatabaseStorageManager {
     }
 
     public void addProduct(String userId, Product newProduct) {
-        FirebaseDatabase.getInstance().getReference().child("Users").child(userId).child("Storage").child(userId + newProduct.getIdCode()).setValue(newProduct).addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                Toast.makeText(context,
-                        "Product added successfully!", Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(context, "Failed to add Product!",
-                        Toast.LENGTH_LONG).show();
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    boolean found = false;
+                    for (DataSnapshot product : snapshot.getChildren()) {
+                        Product currProduct = product.getValue(Product.class);
+                        currProduct.setIdCode(product.getKey());
+                        if(currProduct.equals(newProduct)) {
+                            found = true;
+                            FirebaseDatabase.getInstance().getReference().child("Users").child(userId).child("Storage").child(product.getKey()).child("quantity").setValue(currProduct.getQuantity() + newProduct.getQuantity());
+                            break;
+                        }
+                    }
+
+                    if(found == false) {
+                        FirebaseDatabase.getInstance().getReference().child("Users").child(userId).child("Storage").child(userId + newProduct.getIdCode()).setValue(newProduct).addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(context,
+                                        "Product added successfully!", Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(context, "Failed to add Product!",
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    }
+                }
             }
-        });
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+
+        FirebaseDatabase.getInstance().getReference().child("Users").child(userId).child("Storage").addListenerForSingleValueEvent(valueEventListener);
     }
 
     public void modifyProductQuantity(String userId, Product product) {
